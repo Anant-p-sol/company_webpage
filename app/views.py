c497d213b887946a6fa9e4b12d65de192b353241
Anant kumar <adwayaanant@gmail.com>
2025-07-06 20:19:42 +0530

    adding file in HTML

--- app/views.py
+++ app/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from datetime import datetime
 
 
 # Create your views here.
 def index(request):
-    return render(request,"index.html",{})
+    context = {
+        'course_title':'Django Course',
+        'current_date': datetime.now(),
+        'user' : {
+            'name':'Anant',
+            'email': 'anant372003@gmail.com',
+        
+        },
+        'product_price':9999.999999,
+        'random_text': 'wanna run and explore your Django database in VS Code like a pro? Say less! I got you covered step-by-step',
+
+    }
+
+    return render(request,"index.html",context)
